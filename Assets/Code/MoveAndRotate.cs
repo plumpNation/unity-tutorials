7d8b6f7cb4ab5aaf5734d257401f62ce81312b73
Gavin King <gavin.king@acando.no>
2019-04-18 18:44:22 +0200

    Lesson 5 - player input

--- Assets/Code/MoveAndRotate.cs
+++ Assets/Code/MoveAndRotate.cs
@@ -4,36 +4,11 @@ using UnityEngine;
 
 public class MoveAndRotate : MonoBehaviour {
 
-  // translation floats represent unity units that are roughly 1 metre
-
-  [SerializeField]
-  Vector3 movementVector = new Vector3(1.0f, 0.0f, 0.0f);
-
-  [SerializeField]
-  float rotateX = 0.2f;
-
-  [SerializeField]
-  float rotateY = 1f;
-
-  [SerializeField]
-  float rotateZ = 2.5f;
-
-  [SerializeField]
-  float currentScale = 1.0f;
-
-  [SerializeField]
-  float scaleChangePerFrame = 0.001f;
-
   [SerializeField]
   float speed = 1f;
 
   [SerializeField]
-  bool useSpeed = false;
-
-  // use the world as the origin, and not the object
-
-  [SerializeField]
-  Space transformSpace = Space.World;
+  bool scaleToTime = false;
 
   // Use this for initialization
   void Start () {
@@ -42,43 +17,28 @@ public class MoveAndRotate : MonoBehaviour {
 
   // Update is called once per frame
   void Update () {
-    // time between each update/frame
-    speed = Time.deltaTime;
-
-    Scale();
-    Move();
-    Rotate();
-
-    currentScale = currentScale + scaleChangePerFrame;
-  }
-
-  /// <summary>
-  /// This method will make the object scale
-  public void Scale() {
-    // rotation floats represents degrees
-    Vector3 scaleVector = new Vector3(currentScale, currentScale, currentScale);
-
-    transform.localScale = scaleVector;
+    if (scaleToTime) {
+      speed = Time.deltaTime;
+    }
   }
 
   /// <summary>
   /// This method will make the object rotate
-  public void Rotate() {
+  public void Rotate(float rotateX, float rotateY) {
     // rotation floats represents degrees
-    Vector3 rotationVector = new Vector3(rotateX, rotateY, rotateZ);
+    Vector3 rotationVector = new Vector3(rotateX, rotateY, 0.0f);
 
-    transform.Rotate(rotationVector, transformSpace);
+    transform.Rotate(rotationVector);
   }
 
   /// <summary>
-  /// This method will make the object move
-  public void Move() {
-    Vector3 movingVector = movementVector;
+  /// This method will make the attached object move along the x, y, z every
+  /// time it is called.
+  public void Move(float moveX, float moveY) {
+    Vector3 movingVector = new Vector3(moveX, moveY, 0.0f);
 
-    if (useSpeed) {
-      movingVector = movingVector * speed;
-    }
+    movingVector *= speed;
 
-    transform.Translate(movingVector, transformSpace);
+    transform.Translate(movingVector);
   }
 }
--- Assets/Code/PlayerInput.cs
+++ Assets/Code/PlayerInput.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour {
+  MoveAndRotate moveComponent;
 
   // Use this for initialization
   void Start () {
+    // initialise MoveAndRotate component
+    moveComponent = this.GetComponent<MoveAndRotate>();
+
+    if (moveComponent == null) {
+      Debug.LogError("MoveAndRotate may not be attached to this game object");
+    }
 
   }
 
-  // Update is called once per frame
+  /// <summary>
+  /// In each frame we will grab input values and call appropriate methods
   void Update () {
+    float moveX = Input.GetAxis("MovementX");
+    float moveY = Input.GetAxis("MovementY");
+    float rotateX = Input.GetAxis("RotateX");
+    float rotateY = Input.GetAxis("RotateY");
 
+    bool pressed = Input.GetButtonDown("Pulse");
+
+    moveComponent.Move(moveX, moveY);
+    moveComponent.Rotate(rotateX, rotateY);
+
+    if (pressed) {
+      Pulse();
+    }
+  }
+
+  void Pulse() {
+    Debug.Log("PULSE!");
   }
 }
