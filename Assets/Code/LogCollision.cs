689593fc483a196a2f0067f2950af1f7ab7eb4d2
Gavin King <gavin.king@acando.no>
2019-04-18 22:09:14 +0200

    Lesson 8 - Collisions and triggers

---
+++ Assets/Code/LogCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogCollision : MonoBehaviour {
+
+  void OnCollisionEnter(Collision collision) {
+    string otherObject = collision.gameObject.name;
+
+    Debug.Log(otherObject);
+  }
+
+  // Use this for initialization
+  void Start () {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
+}
---
+++ Assets/Code/LogTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogTrigger : MonoBehaviour {
+  private void OnTriggerEnter(Collider other) {
+    Debug.Log("entering " + other.gameObject.name);
+  }
+
+  private void OnTriggerExit(Collider other) {
+    Debug.Log("exiting " + other.gameObject.name);
+  }
+}
